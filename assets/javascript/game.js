107d43b8d0c64e5cbb5bd1e2dec5ff193b87d2f2
Jorge Sierra <3d.jorgesierra@gmail.com>
2019-02-20 23:06:23 -0500

    fixed some code for clarity

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,12 @@
+/*---------Global Variables ------------*/
+
 var wordCHoices = ["madonna", "michael jackson", "george michael"];
 var randomChoice = wordCHoices[Math.floor(Math.random() * wordCHoices.length)];
 var answer = [];
 var badGuesses = [];
 var lives;
-//var matchWord = wordCHoices.includes(userInput);
+
+/*--------- Reset function ------------*/
 
 function blankValues() {
 
@@ -20,75 +23,66 @@ function blankValues() {
     //Print to the DOM
     document.getElementById("answer").innerHTML = answer.join(" ");
     document.getElementById("lives").innerHTML = lives;
+}
 
+/*--------- User is correct function ------------*/
+var rightLetter = function () {
 
 }
 
+/*--------- User is wrong function ------------*/
+
+var wrongLetter = function (letter) {
+
+    // badGuesses.push(randomChoice[i]);
+    //check if the letter is used 
+
+    if (badGuesses.indexOf(letter) > -1) {
+        alert("You've already pressed that");
+        return;
+    }
+
+    badGuesses.push(letter);
+    document.getElementById("badGuesses").innerHTML = badGuesses.join(" ");
+
+    // update lives and display
+    --lives;
+    document.getElementById("lives").innerHTML = lives;
+    
+    // if lives == 0 then game is over and reset value
+    if (lives === 0) {
+        alert("You have lost!");
+        blankValues();
+
+    }
+}
+
+/*----------call Function Area ------------*/
+
 blankValues();
 
-// USER INPUT
+/*--------- one key up Function ------------*/
 document.onkeyup = function (event) {
 
     //get user input
     var userInput = event.key;
 
-    // is correct function
-    var rightLetter = function (letter) {
-
-
-        console.log("this is randomChoice[i] " + randomChoice[i]);
-        console.log("this is letter " + letter);
-        // answer[i] = letter;
-        document.getElementById("answer").innerHTML = answer.join(" ");
-
-    }
-
-    //is wrong function
-
-    var wrongLetter = function (letter) {
-
-        // badGuesses.push(randomChoice[i]);
-        //check if the letter is used 
-
-        if (badGuesses.indexOf(letter) > -1) {
-            alert("You've already pressed that");
-            return;
-        }
-
-        badGuesses.push(letter);
-        document.getElementById("badGuesses").innerHTML = badGuesses.join(" ");
-
-        // update lives and display
-        --lives;
-        document.getElementById("lives").innerHTML = lives;
-
-        if (lives === 0) {
-            alert("You have lost!");
-        }
-    }
-
-
-    //looking for the position of every letter
+    //create a boolean to stop the wrongLetter function for continously looping
     var found = false;
 
-
+    //index of every letter
     for (var i = 0; i < randomChoice.length; i++) {
         if (userInput === randomChoice[i]) {
             found = true;
             console.log("entered true statement here is the user input: " + userInput);
             answer[i] = userInput;
             document.getElementById("answer").innerHTML = answer.join(" ");
-            console.log("Inside the true statement: " + userInput);
+            console.log("Inside the true statement: " + userInput); 
         }
     }
 
     if (!found) {
         wrongLetter(userInput);
     }
-
-
-
-    /* CREATE A FUNCTION THAT HANDLES FOR LOOP CHECK OF LETTERS*/
-
 
 }
