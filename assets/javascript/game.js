82a6ca6f851a111b31689c0196fb7faeb7e89336
Jorge Sierra <3d.jorgesierra@gmail.com>
2021-01-23 18:02:50 -0500

    made some slight changes, no major refactor

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,216 +1,199 @@
 /*---------Global Variables ------------*/
 
-var wordChoices = ["Tetris",
-    "Super Mario Bros",
-    "Sonic the Hedgehog",
-    "Super Mario Bros",
-    "Donkey Kong",
-    "Pokemon",
-    "The Legend of Zelda",
-    "Rampage",
-    "Metroid",
-    "Prince of Persia",
-    "R Type",
-    "Mega Man",
-    "Pac Man",
-    "Dig Dug",
-    "Final Fantasy",
-    "Punch Out",
-    "Duck Hunt",
-    "Streets of Rage",
-    "Contra"
-];
-var randomChoice = wordChoices[Math.floor(Math.random() * wordChoices.length)].toLowerCase();
+var wordChoices = ['Tetris', 'Super Mario Bros', 'Sonic the Hedgehog', 'Super Mario Bros', 'Donkey Kong', 'Pokemon', 'The Legend of Zelda', 'Rampage', 'Metroid', 'Prince of Persia', 'R-Type', 'Mega Man', 'Pac Man', 'Dig Dug', 'Final Fantasy', 'Punch Out', 'Duck Hunt', 'Streets of Rage', 'Contra'];
+var randomChoice = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+console.log('randomChoice:', randomChoice)
+var randomChoiceIndex = wordChoices.indexOf(randomChoice);
 var answer = [];
 var badGuesses = [];
 var lives = 7;
-var warningMessage = document.getElementById("warningMessage");
-var warningMessageSubText = document.getElementById("warningMessageSubText");
-var welcomeScreen = document.getElementById("welcomeScreen");
-var myAudio = document.getElementById("myAudio");
-var wins =  document.getElementById("wins");
-var losses =  document.getElementById("losses");
+var warningMessage = document.getElementById('warningMessage');
+var warningMessageSubText = document.getElementById('warningMessageSubText');
+var welcomeScreen = document.getElementById('welcomeScreen');
+var myAudio = document.getElementById('myAudio');
+var wins = document.getElementById('wins');
+var losses = document.getElementById('losses');
 var playGameBool = false;
 var winsCount = 0;
-var lossesCount = 0
+var lossesCount = 0;
 
 /*--------- Reset function ------------*/
 
-function blankValues() {
+function resetValues() {
+	// reset variables
+	lives = 7;
+	badGuesses = [];
+	document.getElementById('badGuesses').innerHTML = '';
 
-    // reset variables
-    answer = [];
-    lives = 7;
-    badGuesses = [];
-    document.getElementById("badGuesses").innerHTML = "";
+    //removed current item
+    console.log(randomChoiceIndex);
+    wordChoices.splice(wordChoices.indexOf(randomChoiceIndex));
 
     //reinitialize randomChoice
-    randomChoice = wordChoices[Math.floor(Math.random() * wordChoices.length)].toLowerCase();
+    randomChoice = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+	randomChoiceIndex = wordChoices.indexOf(randomChoice);
 
-    //create blank spaces
-    for (var i = 0; i < randomChoice.length; i++) {
+    console.log(wordChoices);
 
-        //check  if the indexOf is a space and if it is the answer equals that array
-        if (randomChoice[i] === " ") {
-            answer[i] = " ";
-        } else {
+    //fill blanks
+    fillBlanks(randomChoice);
+    console.log(answer);
+	//Print to the DOM
 
-            answer[i] = "_";
-        }
-
-    }
-
-    //Print to the DOM
-    document.getElementById("answer").innerHTML = answer.join("");
-    document.getElementById("lives").innerHTML = lives;
-    document.getElementById("wins").innerHTML = winsCount;
-    document.getElementById("losses").innerHTML = lossesCount;
-    bgMusic(randomChoice);
+	document.getElementById('lives').innerHTML = lives;
+	document.getElementById('wins').innerHTML = winsCount;
+    document.getElementById('losses').innerHTML = lossesCount;
+    console.log(answer);
+	bgMusic(randomChoice);
 }
 
 /*--------- User is wrong function ------------*/
 
 var wrongLetter = function (letter) {
+	//check if the letter is used
 
-    // badGuesses.push(randomChoice[i]);
-    //check if the letter is used 
+	if (badGuesses.indexOf(letter) > -1) {
+		warningMessageSubText.innerHTML = "You've already pressed that!";
+		warningMessageAnimation();
+		return;
+	}
 
-    if (badGuesses.indexOf(letter) > -1) {
-        warningMessageSubText.innerHTML = "You've already pressed that!";
-        warningMessageAnimation();
-        return;
-    }
+	badGuesses.push(letter);
+	document.getElementById('badGuesses').innerHTML = badGuesses.join(' ');
 
-    badGuesses.push(letter);
-    document.getElementById("badGuesses").innerHTML = badGuesses.join(" ");
+	// update lives and display
+	--lives;
 
-    // update lives and display
-    --lives;
+	document.getElementById('lives').innerHTML = lives;
+	losses.innerHTML = lossesCount;
 
-    
-    document.getElementById("lives").innerHTML = lives;
-    losses.innerHTML = lossesCount;
-
-    // if lives == 0 then game is over and reset value
-    if (lives === 0) {
-        myAudio.setAttribute("src", " ");
-        ++lossesCount;
-        losses.innerHTML = lossesCount
-        alert("You have lost!");
-        blankValues();
-    }
-}
+	// if lives == 0 then game is over and reset value
+	if (lives === 0) {
+		myAudio.setAttribute('src', ' ');
+		++lossesCount;
+		losses.innerHTML = lossesCount;
+		alert('You have lost!');
+		resetValues();
+	}
+};
 
 /*----------Audio Function Area ------------*/
 function bgMusic(music) {
-    //
-    music = randomChoice;
-    //var randomMusic Audio("./assets/audio/"+music+".ogg";
-    var randomMusic = "./assets/audio/" + music + ".ogg";
-    // console.log(randomMusic);
+	//
+	music = randomChoice;
+	//var randomMusic Audio("./assets/audio/"+music+".ogg";
+	var randomMusic = './assets/audio/' + music + '.ogg';
+	// console.log(randomMusic);
 
-    myAudio.setAttribute("src", randomMusic);
+	myAudio.setAttribute('src', randomMusic);
 }
 
 /*---------- warning Animation ------------*/
 
 function warningMessageAnimation() {
-    var pos = 0;
-    var id = setInterval(frame, 5);
+	var pos = 0;
+	var id = setInterval(frame, 5);
 
-    function frame() {
-        if (pos === 5) {
-            clearInterval(id);
-        } else {
-            pos++;
-            warningMessage.style.bottom = pos + "vh";
-            warningMessage.style.opacity = "20";
-        }
+	function frame() {
+		if (pos === 5) {
+			clearInterval(id);
+		} else {
+			pos++;
+			warningMessage.style.bottom = pos + 'vh';
+			warningMessage.style.opacity = '20';
+		}
+	}
+}
+
+function fillBlanks(randomChoice) {
+	for (var i = 0; i < randomChoice.length; i++) {
+		//check if the indexOf is a space and if it is the answer equals that array
+		if (randomChoice[i] === ' ') {
+			answer[i] = ' ';
+		} else if (randomChoice[i] === '-') {
+			answer[i] = '-';
+		} else {
+			answer[i] = '_';
+		}
     }
+
+    document.getElementById('answer').innerHTML = answer.join('');
 }
 
 /*----------call Function Area ------------*/
-blankValues();
+fillBlanks(randomChoice);
 playGame();
 
 /*---------- Play Game function ------------*/
 
 function playGame() {
+	document.onkeyup = function (event) {
+		var userInputKeyCode = event.keyCode;
+		if (userInputKeyCode === 32) {
+			setTimeout(function () {
+				playGameBool = true;
+				welcomeScreen.style.display = 'none';
+			}),
+				250;
+		}
+		if (playGameBool) {
+			if (wordChoices.length === 0) {
+				alert('Game has ended!');
+			}
+			//get user input
+			var userInput = event.key.toLowerCase();
 
+			//if user presses F5 or F12 do nothing
+			if (userInputKeyCode === 116 || userInputKeyCode === 123) {
+				return;
+			}
 
-    document.onkeyup = function (event) {
-        var userInputKeyCode = event.keyCode;
-        if (userInputKeyCode === 32) {
+			// check to see if the user input a character from A-Z
+			if (userInputKeyCode > 90 || userInputKeyCode < 57) {
+				warningMessageSubText.innerHTML = ' Please press a leter from A-Z';
+				//warningMessage.setAttribute("class", warningMessage.getAttribute("class")+ " fade-in");
+				warningMessageAnimation();
+			} else {
+				//create a boolean to stop the wrongLetter function for continously looping
+				var found = false;
+				warningMessage.style.opacity = '0';
 
-            setTimeout(function () {
-                playGameBool = true;
-                welcomeScreen.style.display = "none";
-            }), 250;
+				//index of every letter
+				for (var i = 0; i < randomChoice.length; i++) {
+					if (userInput === randomChoice[i].toLowerCase()) {
+						found = true;
+						//console.log("entered true statement here is the user input: " + userInput);
+						answer[i] = userInput;
+						document.getElementById('answer').innerHTML = answer.join('');
+					}
+				}
 
-        }
-        if (playGameBool) {
+				//user is wrong
+				if (!found) {
+					wrongLetter(userInput);
+				}
 
-            //get user input
-            var userInput = event.key.toLowerCase();
+				//If you guess all the correct words!
+				if (found) {
+					var checkAnswer = answer.toString();
 
-            //if user presses F5 or F12 do nothing
-            if (userInputKeyCode === 116 || userInputKeyCode === 123) {
-                return;
-            }
-
-            // check to see if the user input a character from A-Z
-            if (userInputKeyCode > 90 || userInputKeyCode < 57) {
-                warningMessageSubText.innerHTML = " Please press a leter from A-Z";
-                //warningMessage.setAttribute("class", warningMessage.getAttribute("class")+ " fade-in");
-                warningMessageAnimation();
-
-            } else {
-
-                //create a boolean to stop the wrongLetter function for continously looping
-                var found = false;
-                warningMessage.style.opacity = "0";
-
-                //index of every letter
-                for (var i = 0; i < randomChoice.length; i++) {
-                    if (userInput === randomChoice[i]) {
-
-                        found = true;
-                        //console.log("entered true statement here is the user input: " + userInput);
-                        answer[i] = userInput;
-                        document.getElementById("answer").innerHTML = answer.join("");
-                    }
-                }
-
-                //user is wrong
-                if (!found) {
-                    wrongLetter(userInput);
-                }
-
-                //If you guess all the correct words!
-                if (found) {
-                    var checkAnswer = answer.toString();
-
-                    for (var i = 0; i < randomChoice.length; i++) {
-                        //answer is an array
-                        // console.log(checkAnswer);
-                        checkAnswer = checkAnswer.replace(/,/g, "");
-                        checkAnswer = checkAnswer.replace(/_/g, " ");
-                    }
-                    if (checkAnswer === randomChoice) {
-                       
-                        ++winsCount;
-                        wins.innerHTML = winsCount;
-                        setTimeout(function () {             
-                            alert("You won!");
-                            blankValues();
-                        }, 250);
-
-                    }
-
-                }
-
-            }
-        }
-    }
-
+					for (var i = 0; i < randomChoice.length; i++) {
+						//answer is an array
+						// console.log(checkAnswer);
+						checkAnswer = checkAnswer.replace(/,/g, '');
+						checkAnswer = checkAnswer.replace(/_/g, ' ');
+					}
+					if (checkAnswer === randomChoice.toLowerCase()) {
+						++winsCount;
+						wins.innerHTML = winsCount;
+						console.log('wordChoices:', wordChoices)
+						setTimeout(function () {
+							alert('You won!');
+							resetValues();
+						}, 250);
+					}
+				}
+			}
+		}
+	};
 }
